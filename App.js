b3d18795d47d0765edc4d71f0bf19f10ead9b7e9
Kundrapu Naresh <129973630+nareshkundrapu123@users.noreply.github.com>
2024-12-27 17:48:34 +0530

    episode-2

---
+++ App.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const parent=React.createElement("h1",{},"this is naresh from react");
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
